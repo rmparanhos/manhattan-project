6a276e60c6c1730bc59cd9e88e16328edf00f418
Raffael M. Paranhos <raffaelmp@id.uff.br>
2021-11-26 16:11:31 -0300

    Add files via upload

--- script.py
+++ script.py
@@ -46,5 +46,38 @@ def relationship_maker(row):
                         print("2020{},2021{}".format(info['BBL'],item['BBL']))
                 count += 1    
 
+def relationship_maker_by_block(block_number, borough_name):
+    with shapefile.Reader("nyc_mappluto_20v8_arc_shp/MapPLUTO.shp") as shp_2020, shapefile.Reader("nyc_mappluto_21v3_shp/MapPLUTO.shp") as shp_2021, open('nodes.csv', 'a') as nodes_csv, open('edges.csv','a') as edges_csv:
+        index = 0
+        nodes_list = []
+        for record_row in shp_2020.records():
+            if record_row['Block'] == block_number and record_row['Borough'] == borough_name:
+                forma_shapely = shape(shp_2020.shape(index).__geo_interface__)
+                if "2020{}".format(record_row['BBL']) not in nodes_list:
+                    nodes_list.append("2020{}".format(record_row['BBL']))
+                    nodes_csv.write("2020{},{},{},{},2020\n".format(record_row['BBL'],record_row['Borough'],record_row['Block'],record_row['Address']))
+                    print("2020{},{},{},{},2020".format(record_row['BBL'],record_row['Borough'],record_row['Block'],record_row['Address']))
+                index_aux = 0
+                for record_row_aux in shp_2021.records():
+                    if record_row_aux['Block'] == block_number and record_row_aux['Borough'] == borough_name:
+                        if "2021{}".format(record_row_aux['BBL']) not in nodes_list:
+                            nodes_list.append("2021{}".format(record_row_aux['BBL']))
+                            nodes_csv.write("2021{},{},{},{},2021\n".format(record_row_aux['BBL'],record_row_aux['Borough'],record_row_aux['Block'],record_row_aux['Address']))
+                            print("2021{},{},{},{},2021".format(record_row_aux['BBL'],record_row_aux['Borough'],record_row_aux['Block'],record_row_aux['Address']))
+                        forma_shapely_aux = shape(shp_2021.shape(index_aux).__geo_interface__)
+                        if(forma_shapely.intersects(forma_shapely_aux)):
+                            area = forma_shapely.intersection(forma_shapely_aux).area
+                            if area > 0:
+                                #registrando apenas intersecao com area maior q zero (estranho, mas o intersects ta dando true para intersecao com area zero)
+                                edges_csv.write("2020{},2021{},{}\n".format(record_row['BBL'],record_row_aux['BBL'],area))
+                                print("2020{},2021{},{}".format(record_row['BBL'],record_row_aux['BBL'],area))
+                    index_aux += 1
+            index += 1        
+        
 
-relationship_maker(5)               
+#relationship_maker(5)
+relationship_maker_by_block(6, 'MN')    
+relationship_maker_by_block(7, 'MN')           
+relationship_maker_by_block(8, 'MN')
+relationship_maker_by_block(9, 'MN')
+relationship_maker_by_block(10, 'MN')
